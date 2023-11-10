a9f9ba5ef9ab157e099a6e4063657726a5c5e8f3
ayyasmumtaz <yazmumtaz@gmail.com>
2023-11-10 17:03:45 +0700

    create and connect to mongoDB

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 1234;
+
+export const mongoDBURL = 
+    'mongodb+srv://yazmumtaz:nyoba@bookstore.nvps4hm.mongodb.net/books-collection?retryWrites=true&w=majority'
--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,21 @@
-import express, { request, response } from "express";
-import { PORT } from "./config.js";
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
 app.get('/', (request, response) => {
     console.log(request)
-    return response.status(234).send(`Gabut SO`)
+    return response.status(234).send(`Pen jadi backedn developer`)
 })
 
-app.listen(PORT, () => {
-    console.log(`This is PORT : ${PORT}`);
-})
+mongoose.
+    connect(mongoDBURL).
+    then(() => {
+        console.log("Succes")
+        app.listen(PORT, () => {
+            console.log(`This is PORT : ${PORT}`);
+        })
+    }).catch((error) => {
+        console.log(error);
+    })
