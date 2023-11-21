e761632de43a90e69d6e9e7cf82846bf57a15702
ayyasmumtaz <yazmumtaz@gmail.com>
2023-11-21 08:31:24 +0700

    add fetching data function

--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -1,6 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react'
+import BackButton from '../components/BackButton'
+import Spinner from '../components/Spinner'
+import axios from 'axios'
+import { useNavigate, useParams } from 'react-router-dom'
+
 
 const EditBook = () => {
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [publishYear, setPublishYear] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  const {id} = useParams();
+  const navigate = useNavigate();
+
+  const handleEditBook = () => {
+    const data = {
+      title,
+      author,
+      publishYear
+    }
+    setLoading(true);
+    axios
+      .put(`http://localhost:5555/books/${id}`, data)
+      .then(() => {
+        setLoading(false);
+        navigate('/');
+      })
+      .catch((error) => {
+        setLoading(false);
+        alert('An error happened');
+        console.log(error)
+      })
+      
+  }
+
   return (
     <div>EditBook</div>
   )
